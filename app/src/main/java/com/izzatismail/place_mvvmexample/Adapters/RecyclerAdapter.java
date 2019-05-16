f8bcd2e33675ce2a4b7b6abec32b3654bc6f6714
izzatismail <mizzat1996@gmail.com>
2019-05-16 14:44:52 +0800

    Created Recyclerview Layout

---
+++ app/src/main/java/com/izzatismail/place_mvvmexample/Adapters/RecyclerAdapter.java
@@ -0,0 +1,83 @@
+package com.izzatismail.place_mvvmexample.Adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.izzatismail.place_mvvmexample.R;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerAdapter";
+    private ArrayList<String> mImageName = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerAdapter(Context context, ArrayList<String> mImageName, ArrayList<String> mImages) {
+        this.mImageName = mImageName;
+        this.mImages = mImages;
+        this.mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_list_item, viewGroup, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
+        Log.d(TAG, "onBindViewHolder: called ");
+
+        //For circle image
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImages.get(i))
+                .into(viewHolder.imageView);
+
+        viewHolder.imageName.setText(mImageName.get(i));
+        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on " + mImageName.get(i));
+
+                Toast.makeText(mContext, mImageName.get(i),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageName.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView imageView;
+        TextView imageName;
+        RelativeLayout parentLayout;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.imagePlace);
+            imageName= itemView.findViewById(R.id.imageName);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
--- app/src/main/java/com/izzatismail/place_mvvmexample/MainActivity.java
+++ app/src/main/java/com/izzatismail/place_mvvmexample/MainActivity.java
@@ -2,12 +2,73 @@ package com.izzatismail.place_mvvmexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.izzatismail.place_mvvmexample.Adapters.RecyclerAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    //Vars
+    private ArrayList<String> mImageNames = new ArrayList<>();
+    private ArrayList<String> mImageURI = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: started");
+
+
+        initImageBitmaps();
     }
+
+    private void initImageBitmaps() {
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+
+        mImageURI.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
+        mImageNames.add("Havasu Falls");
+
+        mImageURI.add("https://i.redd.it/tpsnoz5bzo501.jpg");
+        mImageNames.add("Trondheim");
+
+        mImageURI.add("https://i.redd.it/qn7f9oqu7o501.jpg");
+        mImageNames.add("Portugal");
+
+        mImageURI.add("https://i.redd.it/j6myfqglup501.jpg");
+        mImageNames.add("Rocky Mountain National Park");
+
+
+        mImageURI.add("https://i.redd.it/0h2gm1ix6p501.jpg");
+        mImageNames.add("Mahahual");
+
+        mImageURI.add("https://i.redd.it/k98uzl68eh501.jpg");
+        mImageNames.add("Frozen Lake");
+
+
+        mImageURI.add("https://i.redd.it/glin0nwndo501.jpg");
+        mImageNames.add("White Sands Desert");
+
+        mImageURI.add("https://i.redd.it/obx4zydshg601.jpg");
+        mImageNames.add("Austrailia");
+
+        mImageURI.add("https://i.imgur.com/ZcLLrkY.jpg");
+        mImageNames.add("Washington");
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: init recyclerview.");
+        RecyclerView recyclerView = findViewById(R.id.place_recycler);
+        RecyclerAdapter adapter = new RecyclerAdapter(this, mImageNames, mImageURI);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
 }
